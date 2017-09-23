0dca5a2175ab0d5602f613bd74d652d3dfeea5f3
chensienyong <ashxcellent_kingzr@yahoo.com>
2017-09-24 00:49:53 +0700

    use api

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-const axios = require('axios')
+const TelegramBot = require('node-telegram-bot-api');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '434005738:AAE7NECtywSBuT7Fq8aiIlj2bGmruCW3h_U';
 
-//This is the route the API will call
-app.post('/new-message', function(req, res) {
-  const message = req.body.message
-  console.log(message)
+const bot = new TelegramBot(token, {polling: true});
 
-  if (!message || message.text.toLowerCase().indexOf('marco') <0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
+bot.on('message', (msg) => {
+  var Hi = "hi";
+  if (msg.text.toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
   }
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios.post('https://api.telegram.org/bot434005738:AAE7NECtywSBuT7Fq8aiIlj2bGmruCW3h_U/sendMessage', {
-    chat_id: message.chat.id,
-    text: 'Polo!!'
-  })
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
-});
-
-// Finally, start our server
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Telegram app listening on port 3000!');
 });
